b8b5cd69e78576d189b5ed34dbc93a1756eec44e
m0v1e_1337 <m0v1e_1337@GODSPADS>
2024-09-09 22:32:22 +0300

    Добавьте файлы проекта.

---
+++ Parol/Passwd.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Parol
+{
+    public class PasswordChecker
+    {
+        public int CheckPassword(string password)
+        {
+            int score = 0;
+            if (Regex.IsMatch(password, @"\d"))
+                score++;
+            if (Regex.IsMatch(password, @"[a-z]"))
+                score++;
+            if (Regex.IsMatch(password, @"[A-Z]"))
+                score++;
+            if (Regex.IsMatch(password, @"\W"))
+                score++;
+            if (password.Length > 10)
+                score++;
+            return score;
+        }
+
+        public static void Main(string[] args)
+        {
+            string[] passwords = { "Password123", "password123", "PASSWORD123", "Password!123", "LongPassword123!", "short" };
+
+            PasswordChecker passwordChecker = new PasswordChecker();
+
+            foreach (string password in passwords)
+            {
+                int score = passwordChecker.CheckPassword(password);
+                Console.WriteLine($"Пароль: {password} - Счет: {score}");
+            }
+
+            Console.WriteLine("Нажмите Enter чтобы выйти...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Passwd.Tests/PasswdTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Parol.Tests
+{
+    [TestFixture]
+    public class PasswordCheckerTests
+    {
+        private PasswordChecker _passwordChecker;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _passwordChecker = new PasswordChecker();
+        }
+
+        [Test]
+        public void TestPasswordWithDigitsAndLowercaseLetters()
+        {
+            int score = _passwordChecker.CheckPassword("Password123");
+            Assert.AreEqual(3, score);
+        }
+
+        [Test]
+        public void TestPasswordWithLowercaseLetters()
+        {
+            int score = _passwordChecker.CheckPassword("password123");
+            Assert.AreEqual(2, score);
+        }
+
+        [Test]
+        public void TestPasswordWithUppercaseLetters()
+        {
+            int score = _passwordChecker.CheckPassword("PASSWORD123");
+            Assert.AreEqual(3, score);
+        }
+
+        [Test]
+        public void TestPasswordWithSpecialCharacters()
+        {
+            int score = _passwordChecker.CheckPassword("Password!123");
+            Assert.AreEqual(4, score);
+        }
+
+        [Test]
+        public void TestPasswordWithMoreThanTenCharacters()
+        {
+            int score = _passwordChecker.CheckPassword("LongPassword123!");
+            Assert.AreEqual(5, score);
+        }
+
+        [Test]
+        public void TestPasswordWithLessThanTenCharacters()
+        {
+            int score = _passwordChecker.CheckPassword("short");
+            Assert.AreEqual(0, score);
+        }
+    }
+}
